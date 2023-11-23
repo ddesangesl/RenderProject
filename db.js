9a67c1cb429cf526c59a49d629fcf1044b65d9ba
desangeslukombo@gmail.com <desanges@LAPTOP-EIO8IS2E>
2023-11-23 11:00:20 +0100

    c2

--- db.js
+++ db.js
@@ -3,6 +3,7 @@ const { Sequelize } = require('sequelize')
 // Database
 const sequelize = new Sequelize(
   '', // TODO: database connection string
+  process.env.KEY_DB,
   {
     dialect: 'postgres',
     dialectOptions: {
